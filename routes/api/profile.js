56fad7dc8ffe522f05fb8cf013535f953d648c77
Sai Kiran Sangam <ssang004@odu.edu>
2022-02-16 14:40:50 -0500

    User Github repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const request = require('request');
+
+const config = require('config');
+
 const router = express.Router();
 
 const auth = require('../../middleware/auth');
@@ -319,6 +323,32 @@ router.delete('/edcuation/:edu_id', auth, async (req, res) => {
 
 }
 );
+
+//@route GET api/profile/github/:username
+//@desc  GET user repos from Github
+//@access public
+
+router.get('/github/:username', (req, res) => {
+    try{
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}
+            /repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}
+            &client_secret=${config.get('githubSecret')}`,
+            method:'GET',
+            headers: { 'user-agent': 'node.js' }
+        };
+        request(options, (error, response, body) => {
+            if(error) console.error(error);
+            if(response.statusCode !== 200){
+                return res.status(404).json({ msg: 'No Github profile found' });
+            }
+            res.json(JSON.parse(body));
+        });
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 
 module.exports = router;
